81646a4f0f4790f896c5d5e67051a050ba386f33
Ahmedhany4 <Ahmed9103hany@gmail.com>
2024-06-20 10:45:10 +0300

    git init

---
+++ day-1/lib/main.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+void main() {
+  // print("Enter Your Name");
+  // String? name = stdin.readLineSync()!;
+  // print("Enter Your Age ") ;
+  // int age = int.parse(stdin.readLineSync()!) ;
+
+  // print("Your Name $name") ;
+  // print("Your Age : ${age * 365 } days");
+
+}//end main
